0c29561dc436b9bfe5c7cfed84c8f4603e1402c9
PameGatica <pam.gatica@profesor.duoc.cl>
2021-06-04 22:52:49 -0400

    django + oracle + panel de admin

---
+++ demo/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Vehiculo(models.Model):
+    patente = models.CharField(primary_key=True,max_length=6, verbose_name='Patente')
+    marca = models.CharField(max_length=20,verbose_name='Marca')
+    modelo = models.CharField(max_length=20, verbose_name='Modelo')
+
+    def __str__(self):
+        return self.patente
+    
